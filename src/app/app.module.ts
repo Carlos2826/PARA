0406ffd5c56094030ec6699e62f5a502632a4f8d
Carlos2826 <carlosjrivera28@hotmail.com>
2024-07-21 04:33:30 -0500

    Bua

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './servicios/auth.service';  // Importar el servicio
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AuthService],  // Agregar el servicio a los proveedores
   bootstrap: [AppComponent],
 })
 export class AppModule {}
