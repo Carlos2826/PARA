de1744041a4167f644c408212928aeb8fd75574a
Carlos2826 <carlosjrivera28@hotmail.com>
2024-07-23 05:33:59 -0500

    SS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://192.168.0.6:8105' // Reemplaza con la IP de tu máquina local
 };
 
 /*
